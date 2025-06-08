fb187774eb66eb1f7014212a52827ed8f5bd4236
Gerard van Engelen <g.vanengelen@codepeople.nl>
2025-06-08 18:27:04 +0200

    fix: Update probes for synthetic monitoring checks to use correct data sources

--- main/main.tf
+++ main/main.tf
@@ -2,7 +2,7 @@ resource "grafana_synthetic_monitoring_check" "Synthetics_BrowserCheck_homepage"
   job       = "Synthetics:BrowserCheck"
   target    = "homepage"
   enabled   = true
-  probes    = [1]
+  probes    = [data.grafana_synthetic_monitoring_probes.main.probes.London]
   labels    = {}
   frequency = 300000
   timeout   = 60000
@@ -16,7 +16,7 @@ resource "grafana_synthetic_monitoring_check" "Synthetics_BrowserCheck_homepage2
   job       = "Synthetics:BrowserCheck2"
   target    = "homepage"
   enabled   = true
-  probes    = [1]
+  probes    = [data.grafana_synthetic_monitoring_probes.main.probes.Frankfurt,]
   labels    = {}
   frequency = 300000
   timeout   = 60000
