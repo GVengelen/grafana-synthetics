2d09013d63c472fca1b8b5f582a8b7c1c5c68516
Gerard van Engelen <g.vanengelen@codepeople.nl>
2025-06-08 11:28:33 +0200

    style: Align formatting in Terraform configuration for consistency

--- main/main.tf
+++ main/main.tf
@@ -1,9 +1,9 @@
 resource "grafana_synthetic_monitoring_check" "Schiphol_BrowserCheck_homepage" {
-  job     = "Schiphol:BrowserCheck"
-  target  = "homepage"
-  enabled = true
-  probes  = [1]
-  labels  = {}
+  job       = "Schiphol:BrowserCheck"
+  target    = "homepage"
+  enabled   = true
+  probes    = [1]
+  labels    = {}
   frequency = 300000
   timeout   = 60000
   settings {
@@ -13,11 +13,11 @@ resource "grafana_synthetic_monitoring_check" "Schiphol_BrowserCheck_homepage" {
   }
 }
 resource "grafana_synthetic_monitoring_check" "Schiphol_BrowserCheck_homepage2" {
-  job     = "Schiphol:BrowserCheck2"
-  target  = "homepage"
-  enabled = true
-  probes  = [1]
-  labels  = {}
+  job       = "Schiphol:BrowserCheck2"
+  target    = "homepage"
+  enabled   = true
+  probes    = [1]
+  labels    = {}
   frequency = 300000
   timeout   = 60000
   settings {
