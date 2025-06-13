456c267760938dc0af53847ab9ff6d96c87e2271
Gerard van Engelen <g.vanengelen@codepeople.nl>
2025-06-13 14:55:16 +0200

    fix: Update Terraform workflows to include credentials token and remove unnecessary variable creation

--- main/versions.tf
+++ main/versions.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
+
+  cloud {
+    organization = "Grafana_Synthetics_Workshop"
+
+    workspaces {
+      name = "grafana-synthetics-main"
+    }
+  }
+
   required_providers {
     grafana = {
       source  = "grafana/grafana"
